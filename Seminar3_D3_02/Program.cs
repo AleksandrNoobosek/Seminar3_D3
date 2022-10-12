30d52aa07d0dca8cbc44bd969d0c82314c441972
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-12 14:51:45 +0300

    add: seminar3_D3_02

---
+++ Seminar3_D3_02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число для вычесления суммы цифр в числе: ");
+int N = int.Parse(Console.ReadLine()??"0");
+
+int count = 0;
+int num = 0; 
+int sum = 0;
+int FirstNum = N; 
+
+if (10<=N)
+{
+    while(count<N)
+        {
+            num=N%10;
+            sum = sum + num;
+            N=N/10;            
+        }
+    Console.WriteLine($"Сумма чисел введенного числа {FirstNum} равна = {sum}");
+}
+
+else
+
+{
+    Console.WriteLine("Подсчет суммы недоступен");
+}
