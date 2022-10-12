8b661279297253ecd6039241fef85dc309199998
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-12 14:52:03 +0300

    add: seminar3_D3_03

---
+++ Seminar3_D3_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число N ");
+double N = double.Parse(Console.ReadLine()?? "0" );
+
+double count = 1;
+double a = 1;
+
+while(count<=N)
+{
+    a=a*count;
+    count++;
+}
+Console.WriteLine($"произведения чисел от 1 до {N} = {a}");
