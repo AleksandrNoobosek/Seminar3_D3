366cf73b1a545d1f2e77913a557debc2f75be4a5
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-12 14:51:25 +0300

    add: seminar3_D3_01

---
+++ Seminar3_D3_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()??"0");
+int count = 1;
+while (count<=N)
+{
+    double kub = Math.Pow(count,3); 
+    Console.WriteLine($"Куб числа {count} = {kub}");
+    count++;          
+}
